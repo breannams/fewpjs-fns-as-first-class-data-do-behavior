88ed2c17819f67ec6c2a434f5c09ebe690bc3a93
breannams <breannamarie_s@hotmail.com>
2021-06-30 22:03:13 -0400

    commit tree 7270b8077fe0f2a2b0c240406047d337f4f68f91

--- index.js
+++ index.js
@@ -10,4 +10,26 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  let timeHours = time.split(':');
+  let timeInput = parseInt(timeHours[0]);
+
+  if (timeInput < 12){
+    return "Good Morning"
+  }
+  if (timeInput > 17){
+    return "Good Evening"
+  }
+  else{
+    return "Good Afternoon"
+  }
+}
+
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(message){
+  let display = document.getElementById('greeting');
+  display.innerText = message
+  return message
+}
